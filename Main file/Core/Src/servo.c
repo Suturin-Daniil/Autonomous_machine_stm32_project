a72a1d90090f6a906e6e66ba5acc7d1082f148a3
Suturin-Daniil <90893260+Suturin-Daniil@users.noreply.github.com>
2022-11-16 13:01:12 +0300

    Update servo.c

--- Main file/Core/Src/servo.c
+++ Main file/Core/Src/servo.c
@@ -2,16 +2,16 @@
 
 uint16_t obrabotka(uint16_t x)
 {
-    return ((1+ (x/4095.))*1000/3 + 1000);
+    return ((1+ (x/4095.))*1000/3 + 1000); // Convert data from joystick to [1333:1666] value
 }
 
 void servoSetPosition(TIM_HandleTypeDef *htim, uint16_t x)
 {
-    htim->Instance->CCR3 = obrabotka(x);
+    htim->Instance->CCR3 = obrabotka(x); // I use CCR3, because of TIM3 PWM Channel is 3
 }
 
 void servoStop(TIM_HandleTypeDef *htim)
 {
-    htim->Instance->CCR1 = 1500;
+    htim->Instance->CCR3 = 1500;
     stopMachine();
 }
