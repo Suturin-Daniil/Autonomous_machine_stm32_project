e172854cc3196cf4c615850b80410bdf91dd8a36
Suturin-Daniil <90893260+Suturin-Daniil@users.noreply.github.com>
2023-06-07 13:11:04 +0300

    Delete Code directory

--- Code/RemoteControl/Core/Inc/joystick.h
+++
@@ -1,8 +0,0 @@
-#include "stm32f4xx_hal.h"
-#include "stdlib.h"
-
-uint16_t getServoValue(ADC_HandleTypeDef* hadc_forServo); // return joystick value for servo
-
-uint16_t getStepValue(ADC_HandleTypeDef* hadc_forMotor); // return joystick value for step motor
-
-uint8_t getStateButton(void); // check states of buttons 
